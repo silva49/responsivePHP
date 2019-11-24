5cbf9adbb08be46d6bceb0255012ec017f599be1
cristian silva <cristiantilanosilva@gmail.com>
2019-11-24 13:07:17 -0500

    https://github.com/silva49/responsivePHP.git

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta charset= "utf-8">
+ <meta name = "viewport" content= "width=device-width, initial-scale=1.0">
+<title>
+ Ejemplo página Responsive Desing
+</title>
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+<div id="pagewrap">
+    
+   <div id="header"> 
+       <div id="info">
+       <h1>Black Friday</h1>  
+       <p>aprovecha nuestras promociones de fin de semana black </p>
+      </div>
+      <div id="logo"> 
+      <img class="imagen1" src="images/black.png">
+      </div>
+    </div> 
+   
+   <div id="content">
+      <h2>Contenido</h2>
+      <p>Párrafo 1</p>
+      <p>Párrafo 2</p>
+      <p>Párrafo 3</p>
+      <p>Párrafo 4</p>
+      <p>Párrafo 5</p>  
+      <p>Párrafo 6</p> 
+    </div>
+   
+   <div  id= "sidebar">
+      <h3>Sidebar</h3>
+      <p>Texo 1</p>
+      <p>Texo 2</p>
+      <p>Texo 3</p>
+   </div>
+
+    <div id= "footer">
+        <h4>Footer</h4>    
+        
+    </div>
+
+</div>
+
+</body>
+</html>
